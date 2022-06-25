2dfb7da947278a8e5956f08a42803da1bd4d602b
Alfredo Pozo <alfredo.pozo@globant.com>
2022-06-25 00:34:53 -0500

    Test change 1

--- src/Program.cs
+++ src/Program.cs
@@ -48,7 +48,8 @@ record Album(int Id, string Title, string Artist, double Price, string Image_url
             new Album(4, "Lost in Translation", "MegaDNS", 12.99,"https://aka.ms/albums-envoylogo"),
             new Album(5, "Lock Down Your Love", "V is for VNET", 12.99, "https://aka.ms/albums-vnetlogo"),
             new Album(6, "Sweet Container O' Mine", "Guns N Probeses", 14.99, "https://aka.ms/albums-containerappslogo"),
-            new Album(7, "Purple Rain", "Prince and the containers", 12.99, "https://aka.ms/albums-vnetlogo")
+            new Album(7, "Purple Rain", "Prince and the containers", 12.99, "https://aka.ms/albums-vnetlogo"),
+            new Album(8, "Listen without containers", "George Maven", 14.99, "https://aka.ms/albums-containerappslogo")
          };
 
         return albums; 
